afdd248d844443ba4fc37705c097cde3f7d4ca85
Peter Zieseniss <peter@zconsulting.net>
2022-08-02 21:44:52 +0200

    Update MainController.php

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -26,8 +26,11 @@ use humhub\modules\karma\models\KarmaSearch;
 use Yii;
 use humhub\modules\bulk_import\forms\BulkImportForm;
 use humhub\modules\user\models\User;
+use humhub\modules\user\models\Profile;
 use humhub\modules\user\models\Password;
 use humhub\modules\space\models\Space;
+use humhub\modules\user\models\Group;
+//use humhub\modules\user\models\GroupUser;
 use yii\helpers\Html;
 use humhub\libs\ProfileImage;
 
@@ -35,53 +38,55 @@ class MainController extends \humhub\modules\admin\components\Controller
 {
 
 
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'acl' => [
-                'class' => \humhub\components\behaviors\AccessControl::className(),
-                'adminOnly' => true
-            ]
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors()
+	{
+		return [
+			'acl' => [
+				'class' => \humhub\components\behaviors\AccessControl::className(),
+				'adminOnly' => true
+			]
+		];
+	}
 
 	/** 
 	 * Registers a user
 	 * @param $data
-     * @return Bool
+	 * @return Bool
 	 */
 	private function registerUser($data) {
+	
+	$MyChosenGroupID = 3;
+	
+		$userModel = new User();
+		$userModel->scenario = 'editAdmin';
 
-        $userModel = new User();
-        $userModel->scenario = 'registration';
-
-        $profileModel = $userModel->profile;
-        $profileModel->scenario = 'registration';
-
-        // User: Set values
-        $userModel->username = $data['username'];
-        $userModel->email = $data['email'];
-        //$userModel->group_id = $data['group_id'];
-        $userModel->status = User::STATUS_ENABLED;
-
-	    // Profile: Set values
-		$profileModel->firstname = $data['firstname'];
-		$profileModel->lastname = $data['lastname'];
+		// User: Set values
+		$userModel->username = $data['username'];
+		$userModel->email = $data['email'];
 		
-
+		$userModel->status = User::STATUS_ENABLED;
+		
+		// Profile: Set values
+		$userProfileModel = $userModel->profile; 
+		$userProfileModel->user_id = $userModel->id;
+		$userProfileModel->scenario = 'editAdmin'; 
+		$userProfileModel->firstname = $data['firstname'];
+		$userProfileModel->lastname = $data['lastname'];
+		$userProfileModel->country = $data['country'];
+		
 		// Password: Set values
 		$userPasswordModel = new Password();
 		$userPasswordModel->setPassword($data['password']);
 
-        if($userModel->save()) {
-
-	        // Save user profile
-			$profileModel->user_id = $userModel->id;
-			$profileModel->save();
-
+		if($userModel->save()) {
+		
+		$userProfileModel->save();
+		
+		Group::findOne(['id' => $MyChosenGroupID])->addUser($userModel->id);
+		
 			// Save user password
 			$userPasswordModel->user_id = $userModel->id;
 			$userPasswordModel->save();
@@ -90,13 +95,13 @@ class MainController extends \humhub\modules\admin\components\Controller
 			foreach ($data['space_names'] as $key => $space_name) {
 
 				// Find space by name attribute
-                $space = Space::findOne(['name'=>$space_name]);
+				$space = Space::findOne(['name'=>$space_name]);
 
 				// Create the space if not found
 				if($space == null) {
 					$space = new Space();
-    				$space->name = $space_name;
-    				$space->save(); 
+					$space->name = $space_name;
+					$space->save(); 
 				}
 
 				// Add member into space
@@ -106,48 +111,48 @@ class MainController extends \humhub\modules\admin\components\Controller
 
 			return true;
 
-        } else {
-            Yii::$app->session->setFlash('error', Html::errorSummary($userModel));
-        	return false;
-        }
+		} else {
+			Yii::$app->session->setFlash('error', Html::errorSummary($userModel));
+			return false;
+		}
 
 	}
 
-    public function actionIndex(){
-    	$form = new BulkImportForm;
+	public function actionIndex(){
+		$form = new BulkImportForm;
 
-        return $this->render('index', array(
-            'model' => $form
-        ));
-    }
+		return $this->render('index', array(
+			'model' => $form
+		));
+	}
 
-    public function actionSetDefaultPass() {
+	public function actionSetDefaultPass() {
 
-    	if(isset($_GET['user_ids'])) {
-    		$user_ids = explode(",", $_GET['user_ids']);
+		if(isset($_GET['user_ids'])) {
+			$user_ids = explode(",", $_GET['user_ids']);
 
-	    	foreach($user_ids as $user_id) {
-	        	$userPasswordModel = new Password();
-	        	$userPasswordModel->user_id = $user_id;
-		        $userPasswordModel->setPassword("password");
-		        
-		        if($userPasswordModel->save()) {
-		        	echo "Saved... <br />";
-		        }
+			foreach($user_ids as $user_id) {
+				$userPasswordModel = new Password();
+				$userPasswordModel->user_id = $user_id;
+				$userPasswordModel->setPassword("password");
+				
+				if($userPasswordModel->save()) {
+					echo "Saved... <br />";
+				}
 
-		    }
-    	} else {
-    		echo "<p>?user_ids=user_id,user_id to reset the password of users to 'password'</p>";
-    	}
-    	
+			}
+		} else {
+			echo "<p>?user_ids=user_id,user_id to reset the password of users to 'password'</p>";
+		}
+		
 
-    }
+	}
 
-    public function actionIdenticon() {
+	public function actionIdenticon() {
 
-//        $assetPrefix = Yii::app()->assetManager->publish(dirname(__FILE__) . '/../assets', true, 0, defined('YII_DEBUG'));
-//        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/md5.min.js');
-//        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/jdenticon-1.3.0.min.js');
+//		$assetPrefix = Yii::app()->assetManager->publish(dirname(__FILE__) . '/../assets', true, 0, defined('YII_DEBUG'));
+//		Yii::app()->clientScript->registerScriptFile($assetPrefix . '/md5.min.js');
+//		Yii::app()->clientScript->registerScriptFile($assetPrefix . '/jdenticon-1.3.0.min.js');
 
 		if(isset($_POST['userids'])) {
 
@@ -163,37 +168,37 @@ class MainController extends \humhub\modules\admin\components\Controller
 			}
 		}
 
-        $searchModel = new \humhub\modules\admin\models\UserSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+		$searchModel = new \humhub\modules\admin\models\UserSearch();
+		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-        return $this->render('identicon', array(
-            'dataProvider' => $dataProvider,
-            'searchModel' => $searchModel
-        ));
+		return $this->render('identicon', array(
+			'dataProvider' => $dataProvider,
+			'searchModel' => $searchModel
+		));
 
-    }
+	}
 
 	public function actionUpload() {
-	    
-//        $assetPrefix = Yii::$app->assetManager->publish(dirname(__FILE__) . '/../assets', array('forceCopy' => true));
-//        Yii::$app->clientScript->registerScriptFile($assetPrefix . '/md5.min.js');
-//        Yii::$app->clientScript->registerScriptFile($assetPrefix . '/jdenticon-1.3.0.min.js');
+		
+//		$assetPrefix = Yii::$app->assetManager->publish(dirname(__FILE__) . '/../assets', array('forceCopy' => true));
+//		Yii::$app->clientScript->registerScriptFile($assetPrefix . '/md5.min.js');
+//		Yii::$app->clientScript->registerScriptFile($assetPrefix . '/jdenticon-1.3.0.min.js');
 		require_once(dirname(__FILE__) . "/../lib/parsecsv.lib.php");
-        $csv = new \parseCSV();
-	    $model = new BulkImportForm;
+		$csv = new \parseCSV();
+		$model = new BulkImportForm;
 
-	    $validImports = array();
-	    $invalidImports = array();
+		$validImports = array();
+		$invalidImports = array();
 
-	    if(isset($_POST['BulkImportForm']))
-	    {
+		if(isset($_POST['BulkImportForm']))
+		{
 
-	        $model->attributes=$_POST['BulkImportForm'];
-	        if(!empty($_FILES['BulkImportForm']['tmp_name']['csv_file']))
-	        {
+			$model->attributes=$_POST['BulkImportForm'];
+			if(!empty($_FILES['BulkImportForm']['tmp_name']['csv_file']))
+			{
 
-	            $file = \yii\web\UploadedFile::getInstance($model,'csv_file');
-	            //$group_id = 1;
+				$file = \yii\web\UploadedFile::getInstance($model,'csv_file');
+				//$group_id = 1;
 
 				$csv->auto($file->tempName);
 				foreach($csv->data as $data) {
@@ -206,62 +211,63 @@ class MainController extends \humhub\modules\admin\components\Controller
 					// Put data into correct format
 				$space_names = array("Default"); //If we don't set an array with at least one element an exception is thrown further down the line.
 				if (array_key_exists('space_names', $data)){
-			    		$space_names = explode(",", $data['space_names']);
+						$space_names = explode(",", $data['space_names']);
 				}
-			    	$importData = array(
-			    		'username' => $data['username'],
-			    		'password' => $data['password'], 
+					$importData = array(
+						'username' => $data['username'],
+						'password' => $data['password'], 
 
-						'firstname' => $data['firstname'],
-			    		'lastname' => $data['lastname'], 
-			    		'email' => $data['email'],
-			    	//	'group_id' => $group_id,
+					'firstname' => $data['firstname'],
+						'lastname' => $data['lastname'], 
+						'email' => $data['email'],
+						'country' => $data['country'],
+					//	'group_id' => $group_id,
 					'space_names' => $space_names,
-			    	);
+					);
 
 
-			    	// Register user
-			    	if($this->registerUser($importData)) {
-			    		$validImports[] = $importData;
-			    	} else {
-			    		$invalidImports[] = $importData;
-			    	}
+					// Register user
+					if($this->registerUser($importData)) {
+						$validImports[] = $importData;
+					} else {
+						$invalidImports[] = $importData;
+					}
 
 				}
 
-	        }
+			}
 
-	    }
+		}
 
-        return $this->render('import_complete', array(
-            'validImports' => $validImports,
-            'invalidImports' => $invalidImports,
-        ));
+		return $this->render('import_complete', array(
+			'validImports' => $validImports,
+			'invalidImports' => $invalidImports,
+		));
 
 	}
 
 
-    /** 
-     * Uploads the identicon profile picture
-     * @param int User ID
-     * @param Base64 Image (identicon)
-     */
-    private function uploadProfilePicture($userId, $data) 
-    {
+	/** 
+	 * Uploads the identicon profile picture
+	 * @param int User ID
+	 * @param Base64 Image (identicon)
+	 */
+	private function uploadProfilePicture($userId, $data) 
+	{
 
-        // Create temporary file
-        $temp_file_name = tempnam(sys_get_temp_dir(), 'img') . '.png';
-        $fp = fopen($temp_file_name,"w");
-        fwrite($fp, file_get_contents($data));
-        fclose($fp);
+		// Create temporary file
+		$temp_file_name = tempnam(sys_get_temp_dir(), 'img') . '.png';
+		$fp = fopen($temp_file_name,"w");
+		fwrite($fp, file_get_contents($data));
+		fclose($fp);
 
-        // Store profile image for user
-        $profileImage = new ProfileImage($userId);
-        $profileImage->setNew($temp_file_name);
+		// Store profile image for user
+		$profileImage = new ProfileImage($userId);
+		$profileImage->setNew($temp_file_name);
 
-        // Remove temporary file
-        unlink($temp_file_name);
+		// Remove temporary file
+		unlink($temp_file_name);
 
-    }
+	}
 
 }
